543b449ad6c743cec34c755a2d2fbb3421284c1d
fais8 <fais8@FAISAL>
2024-05-12 23:35:15 +0300

    Add project files.

---
+++ Store.Domain/Entities/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int ReorderLimit { get; set; }
+        public string UnitPrice { get; set; }
+        public int SockUnit { get; set; }
+        public int OrderUnit { get; set; }
+    }
+}
